abad377934745c45edcc9a131f19d266ae55421c
bryanpb1 <bryan_a21@live.com.mx>
2020-05-19 18:11:27 -0500

    se pueden solucionar problemas directos

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ var capacitanciaMos='La capacitancia se expresa como la relación entre la carga
 var textoPotencial=document.getElementById("textoMostrarPotencial");
 var textoCapacitancia=document.getElementById("textoMostrarCapacitancia");
 
+var carga;
+var trabajoP;
+var diferenciaPo;
+var EnergiaPotencial;
+var distancia=parseFloat(document.getElementById('distanciaP').value);
+
+
+var K;
+
+K=(9)*(Math.pow(10,9))
+
+
+var cargaMostrar=document.getElementById("cargaPot");
+
 function Mostrar()
 {
     if(document.getElementById("caja").style.opacity == 1){
@@ -31,5 +45,140 @@ function MostrarCapacitancia()
         document.getElementById("cajaCap").style.opacity="1";
         textoCapacitancia.innerHTML=capacitanciaMos
     }
+
+}
+
+/////////////////////////////////MOSTRAR CARGA/////////////////////////////////////////
+
+function mostrarCarga(){
+    verificarElevados();
+    var  formulaShow;
+    var formulaPotencia;
+    var NformulaPotencia;
+    var formulaPotenciaDist;
+
+    if(trabajoP>0 && diferenciaPo>0){
+        formulaShow=parseFloat(formulaCarga(trabajoP,diferenciaPo)) ;
+        document.getElementById('cargaPot').value=formulaShow;
+
+    }else if(trabajoP>0 && carga>0){
+        formulaPotencia=parseFloat(formulaPotencial(trabajoP,carga));
+        document.getElementById('diferenciaPotencial').value=formulaPotencia;
+    }else if(EnergiaPotencial>0 && carga>0){
+       NformulaPotencia=parseFloat(formulaPotencialEnergia(EnergiaPotencial,carga));
+        document.getElementById('diferenciaPotencial').value=NformulaPotencia;
+    }else if(distancia>0 && carga>0){
+formulaPotenciaDist=parseFloat(formulaPotencialDistancial(distancia,carga));
+document.getElementById('diferenciaPotencial').value=formulaPotenciaDist;
+
+}
+
+    
+
+  
+location.reload();
+
+}
+
+///////////////////////////////////////FORMULA POTENCIAL//////////////////////////////////////////////
+
+function formulaPotencial(trabajo,cargar)
+{
+   
+        var resultado=trabajo/cargar
+        return resultado
+  
+    
+}
+
+//////////////FORMULA POTENCIAL CON ENERGIA POTENCIAL Y CARGA/////////////////////////////////////////
+function formulaPotencialEnergia(energiaP,cargar)
+{
+   
+        var resultado=energiaP/cargar
+        return resultado
+  
+    
+}
+
+/////////////////////////////FORMULA POTENCIAL CON DISTANCIA//////////////////////////////////////
+function formulaPotencialDistancial(dist,cargar)
+{
+   var mul=K*cargar
+        var resultado=mul/dist
+        return resultado
+
+    
+}
+
+//////////////////////////////////////////FORMULA CARGA////////////////////////////////////////////////
+
+function formulaCarga(trabajo,diferenciaPot)
+{
+   
+        var resultado =trabajo/diferenciaPot
+        return resultado
+    
+    
+}
+
+
+////////////////////////////////////////////VERIFICAR ELEVADOS/////////////////////////////////////
+
+function verificarElevados(){
+     carga=String(document.getElementById("cargaPot").value) ;
+ trabajoP=String(document.getElementById("trabajoPot").value) ;
+ diferenciaPo=String(document.getElementById("diferenciaPotencial").value) ;
+ EnergiaPotencial=String(document.getElementById("energiaPot").value)
+
+if(trabajoP.includes('10'))
+{
+var fields=trabajoP.split('x');
+var uno=fields[0];
+var dos=fields[1];
+var cambio= dos.split('^');
+var c1=cambio[0];
+var c2=cambio[1];
+var tot=Math.pow(c1,c2);
+trabajoP=uno*tot
+}else{trabajoP=parseFloat(document.getElementById("trabajoPot").value);}
+
+
+if(diferenciaPo.includes('10'))
+{
+var fielddif=diferenciaPo.split('x');
+var uno=fielddif[0];
+var dos=fielddif[1];
+var cambio= dos.split('^');
+var c1=cambio[0];
+var c2=cambio[1];
+var tot=Math.pow(c1,c2);
+diferenciaPo=uno*tot
+}else{diferenciaPo=parseFloat(document.getElementById("diferenciaPotencial").value);}
+
+if(carga.includes('10'))
+{
+var fielddif=carga.split('x');
+var uno=fielddif[0];
+var dos=fielddif[1];
+var cambio= dos.split('^');
+var c1=cambio[0];
+var c2=cambio[1];
+var tot=Math.pow(c1,c2);
+carga=uno*tot
+console.log(carga);
+}else{carga=parseFloat(document.getElementById("cargaPot").value);}
+
+if(EnergiaPotencial.includes('10'))
+{
+var fielddif=EnergiaPotencial.split('x');
+var uno=fielddif[0];
+var dos=fielddif[1];
+var cambio= dos.split('^');
+var c1=cambio[0];
+var c2=cambio[1];
+var tot=Math.pow(c1,c2);
+EnergiaPotencial=uno*tot
+}else{EnergiaPotencial=parseFloat(document.getElementById("energiaPot").value);}
 
 }
